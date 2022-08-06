0b0fbd0a7b642014e881eff7b0a22d064221d66a
amefuru <110430986+amefuru@users.noreply.github.com>
2022-08-06 10:04:48 +0900

    stuidy セクション4おわり

--- src/index.js
+++ src/index.js
@@ -10,18 +10,18 @@ document.getElementById("app").innerHTML = `
 `;
 
 // 再代入可能 再代入不可能
-let var1 = "aaa";
-console.log(var1);
+// let var1 = "aaa";
+// console.log(var1);
 
-var1 = "bbb";
-console.log(var1);
+// var1 = "bbb";
+// console.log(var1);
 
 // let var1 = "bbb";
 // console.log(var1);
 
 // 再代入不可能 再代入不可能
-const var2 = "aaa";
-console.log(var2);
+// const var2 = "aaa";
+// console.log(var2);
 
 // var2 = "bbb"
 // console.log(var2);
@@ -30,24 +30,24 @@ console.log(var2);
 // console.log(var2);
 
 // アロー関数
-const funcadd = (a, b) => a + b;
-console.log(funcadd(1, 1));
+// const funcadd = (a, b) => a + b;
+// console.log(funcadd(1, 1));
 
-const petsname = {
-  dog: "サンダー",
-  cat1: "ゆず",
-  car2: "わさび"
-};
+// const petsname = {
+//   dog: "サンダー",
+//   cat1: "ゆず",
+//   cat2: "わさび"
+// };
 
-const { dog, cat1, cat2 } = petsname;
-const message = `ペットの名前は${dog}、${cat1}、${cat2}です`;
-console.log(message);
+// const { dog, cat1, cat2 } = petsname;
+// const message = `ペットの名前は${dog}、${cat1}、${cat2}です`;
+// console.log(message);
 
 // deforuto value
 
-const sayHello = (name = "ゲスト") => console.log(`Hello, ${name}!`);
-sayHello("amefuru");
-sayHello();
+// const sayHello = (name = "ゲスト") => console.log(`Hello, ${name}!`);
+// sayHello("amefuru");
+// sayHello();
 
 // スプレッド構文
 
@@ -78,3 +78,52 @@ sayHello();
 // console.log(arr7);
 
 // イコールで設定すると、参照が被ってしまうため、元の配列が影響をうけるので非推奨
+
+// mapやfilterを使う配列の処理
+
+const nameArr = ["田中", "佐藤", "伊藤"];
+const nameArr2 = nameArr.map((name, index) => {
+  return `${index + 1}は${name}さん`;
+});
+// console.log(nameArr2);
+
+const numArr = [1, 2, 3, 4, 5];
+const numArr2 = numArr.filter((num) => {
+  return num % 2 === 0;
+});
+// console.log(numArr2);
+
+const nameArr3 = nameArr.map((name) => {
+  if (name === "田中") {
+    return name;
+  } else {
+    return `${name}さん`;
+  }
+});
+// console.log(nameArr3);
+
+// 三項演算子
+
+// const var1 = 1 > 0 ? true : false;
+// console.log(var1);
+
+const checkSum = (n1, n2) => {
+  return n1 + n2 > 100 ? "Over100" : "under100";
+};
+console.log(checkSum(10, 10));
+console.log(checkSum(100, 10));
+
+// 論理演算子
+
+// || の左側がfalse時右側を返す
+
+// const num = null; // false
+const num = 100;
+const fee = num || "金額未設定";
+console.log(fee);
+
+// && の左側がtrueの時右側を返す
+// const num2 = null;
+const num2 = 1;
+const fee2 = num2 && "設定";
+console.log(fee2);
